200cfec0354838014c01577ff2d9127ec1c88557
bitan22 <122077843+bitan22@users.noreply.github.com>
2023-01-11 10:43:03 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import abi from "./contracts/chai.json";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Buy from "./components/buy";
+import Memos from "./components/memos";
+import "./App.css";
+
+function App() {
+  const [state, setState] = useState({
+    provider: null,
+    signer: null,
+    contract: null,
+  });
+  const [account, setAccount] = useState("None");
+  useEffect(() => {
+    const connectWallet = async () => {
+      const contractAddress = "0xA9Fb5860fF29Ef3339b9c18d6c09ff4445B89b93";
+      const contractABI = abi.abi;
+      try {
+        const { ethereum } = window;
+
+        if (ethereum) {
+          const account = await ethereum.request({
+            method: "eth_requestAccounts",
+          });
+
+          window.ethereum.on("chainChanged", () => {
+            window.location.reload();
+          });
+
+          window.ethereum.on("accountsChanged", () => {
+            window.location.reload();
+          });
+
+          const provider = new ethers.providers.Web3Provider(window.ethereum);
+          const signer = provider.getSigner(); // remove this
+          const contract = new ethers.Contract(contractAddress,contractABI, signer); // replace signer with provider
+          setAccount(account);
+          setState({ provider, signer, contract });
+        } else {
+          alert("Please install metamask");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    connectWallet();
+  }, []);
+  // console.log(state);
+  return (
+    <div style={{ backgroundColor: "#EFEFEF", height: "100%" }}>
+      <p
+        classname="text-muted lead "
+        style={{ marginTop: "10px", marginLeft: "5px" }}
+      >
+        <small>Connected Account - {account}</small>
+      </p>
+      <div className="container">
+        <Buy state={state} />
+        <Memos state={state}/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
